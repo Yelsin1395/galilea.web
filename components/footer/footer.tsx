010730624158423c8530e263b217e1708c86c16a
Jesús Yelsin Broly <yelgari13@gmail.com>
2024-04-20 14:29:37 -0500

    feat(architecture): init compose project for urb vista alegre.

---
+++ components/footer/footer.tsx
@@ -0,0 +1,14 @@
+export default function Footer() {
+	return (
+		<footer className='footer'>
+			<div className='content has-text-centered'>
+				<p>
+					Realizado por{' '}
+					<a href='https://www.linkedin.com/in/jesusyelsinbroly/' target='__blank'>
+						<strong>Jesús Yelsin Broly García Rivera</strong>
+					</a>
+				</p>
+			</div>
+		</footer>
+	)
+}
